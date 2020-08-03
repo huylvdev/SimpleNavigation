660ae8a428d3a84e1ffa40f462742e1ea5a9e391
Huy <>
2020-08-03 21:13:47 +0700

    first commit navigation

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/HomeScreen'
+import DetailsScreen from './src/DetailsScreen'
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator  >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-SimpleNavigation-SimpleNavigationTests/Pods-SimpleNavigation-SimpleNavigationTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleNavigation_SimpleNavigationTests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleNavigation_SimpleNavigationTests
+@end
